d3c7946bb812f7beb9aa2bb6ed57cb545eb13694
Bartosz Kopiński <bartosz.kopinski@netguru.pl>
2015-01-28 06:34:46 +0100

    Update specs

--- spec/models/plan_spec.rb
+++ spec/models/plan_spec.rb
@@ -46,7 +46,7 @@ describe Plan do
         private_repo = double("Repo", private?: true, exempt?: false)
         plan = Plan.new(private_repo)
 
-        expect(plan.price).to eq 12
+        expect(plan.price).to eq 0
       end
     end
 
